2303fc7d08ecb7eb3fb11401e98193e73db3e867
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-01-27 18:15:42 +0530

    Create wtfProblem2.c

---
+++ wtfProblem2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i ='2';
+    
+    printf("%d", i);
+    
+    if ( i !== '1')
+    printf("Noob");
+
+    return 0;
+}
+
+//OUTPUT : 50Noob
+
+/*
+When an integer is assigned to or compared with a character, the corresponding ASCII value of the latter is considered.
+*/

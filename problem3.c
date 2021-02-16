1cab67f489c48eb7d56492c24ab866d0002e6d8d
Aurav <47893192+le-incroyable1-dev@users.noreply.github.com>
2021-02-16 13:07:12 +0530

    Create problem3.c

---
+++ problem3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[3][3] = {1,2,3,4,5,6,7,8,9};
+  int i,j=0;
+  for(i=0;i<3;i++) {
+  printf("%d",a[i][j+1]);
+  }
+  
+  return 0;
+ 
+} 
+
+/*
+
+OUTPUT : 2 5 8 
+int a[3][3] = {1,2,3,4,5,6,7,8,9};
+IS EQUIVALENT TO
+int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+
+*/
